157cc48eb36210a9eedc1fa4d92900ae9fd9cd0f
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-28 05:18:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,39 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-
-  post '/login' do
-
+  
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
   end
 
-  get '/account' do
-
+  post '/registrations' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:user_id] = @user.id
+    
+    redirect '/users/home'
   end
-
-  get '/logout' do
-
+  
+  get '/sessions/login' do
+    erb :'sessions/login'
   end
-
-
+  
+  post '/sessions' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
+  end
+  
+  get '/sessions/logout' do
+    session.clear
+    redirect '/'
+  end
+  
+  get '/users/home' do
+    @user = user.fin(session[:user_id])
+    erb :'/users/home'
+  end
 end
-
